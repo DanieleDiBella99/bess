a946b0d5b4a01376c5a0540e4fa48af5e632994d
DanieleDiBella99 <danieledibella1999@gmail.com>
2025-04-18 01:39:43 +0200

    Update build.py

--- build.py
+++ build.py
@@ -358,7 +358,7 @@ def build_bess():
     sys.stdout.flush()
     cmd('bin/bessctl daemon stop 2> /dev/null || true', shell=True)
     cmd('rm -f core/bessd')  # force relink as DPDK might have been rebuilt
-    cmd('V=1 make -C core bessd modules all_test %s' % makeflags())
+    cmd('make -C core bessd modules all_test %s' % makeflags())
 
 
 def build_kmod():
